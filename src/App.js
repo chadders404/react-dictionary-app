672420ee505dd4f157b24940df5bd950209e1c4b
chadders404 <chadwick1996@gmail.com>
2022-05-15 02:03:45 +0100

    created Dictionary file and added basic search form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Dictionary</h1>
       </header>
+      <main>
+        <Dictionary />
+      </main>
     </div>
   );
 }
-
-export default App;
