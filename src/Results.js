c6dcc239912b13ffa463e15ba0d6d19724c15449
chadders404 <chadwick1996@gmail.com>
2022-05-16 15:54:08 +0100

    Created Meaning file which maps and returns multiple definitions

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
-import react from "react";
+import React from "react";
+import Meaning from "./Meaning.js";
 
 export default function Results(props) {
   console.log(props.results);
@@ -6,6 +7,14 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
       </div>
     );
   } else {
