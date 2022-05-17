41bc2094b1d56532e2a4c4e96c8522e456f14a97
chadders404 <chadwick1996@gmail.com>
2022-05-17 11:13:55 +0100

    added synonym file to return ul/li of synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,21 +1,27 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
-    <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
-          </div>
-        );
-      })}
+    <div className="meaning">
+      <div>
+        <h3>{props.meaning.partOfSpeech}</h3>
+        {props.meaning.definitions.map(function (definition, index) {
+          return (
+            <div key={index}>
+              <div>
+                <br />
+                {definition.definition}
+                <br />
+                <em>{definition.example}</em>
+                <br />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
