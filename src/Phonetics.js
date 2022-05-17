a4313902f1c9fd1f43d040366c953eeff56d993a
chadders404 <chadwick1996@gmail.com>
2022-05-17 13:08:20 +0100

    attempted fix - API still not returning phonetics array

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 export default function Phonetics(props) {
-  console.log(props.phonetics);
-  if (props.phonetics) {
+  console.log(props.phonetic);
+  if (props.phonetic) {
     return <div className="phonetics">Loading phonetics...</div>;
   } else {
     return null;
